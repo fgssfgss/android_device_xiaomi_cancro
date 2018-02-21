40ab6adece5f86a69da71d275340cf2431ff9797
DrRamm <kotsareu@gmail.com>
2018-02-21 08:39:07 +0000

    cancro: Rework bdaddr
    Change-Id: I16dcb21a5c4aa2cef88a0d336d60177147d1a983

--- bdaddr_xiaomi/Android.mk
+++ bdaddr_xiaomi/Android.mk
@@ -5,5 +5,6 @@ LOCAL_MODULE           := bdaddr_xiaomi
 LOCAL_MODULE_TAGS      := optional
 LOCAL_SRC_FILES        := bdaddr_xiaomi.c
 LOCAL_CFLAGS           += -Wall
-LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog libqminvapi
+LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog libqminvapi libbase
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_EXECUTABLE)
