690abfddcb63f49b5dda0dd536a1c7a17772cd59
DrRamm <kotsareu@gmail.com>
2018-03-21 15:15:48 +0300

    add audio_platform from virgo
    kanged this https://github.com/xiaomi-note-dev/android_device_xiaomi_virgo/blob/b06237b952733812840b7e1e2c846905c91cca66/audio/audio_platform_info.xml
    
    Change-Id: Ib5e02797cbdd7670f8ca4665c444f54c357e4f41

--- cancro.mk
+++ cancro.mk
@@ -153,6 +153,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/listen_platform_info.xml \
     $(LOCAL_PATH)/audio/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
